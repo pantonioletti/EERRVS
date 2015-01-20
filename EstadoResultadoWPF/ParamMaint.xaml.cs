1acc353c485535ff74ca69a69efc6dc8c67dd4d8
Pablo Antonioletti <pablo.antonioletti@gmail.com>
2015-01-20 18:49:12 -0300

    First version reading and writing data

--- EstadoResultadoWPF/ParamMaint.xaml.cs
+++ EstadoResultadoWPF/ParamMaint.xaml.cs
@@ -27,35 +27,30 @@ namespace EstadoResultadoWPF
             InitializeComponent();
             curParam = param;
             this.eerr = eerr;
+            initialize();
         }
 
         private void initialize()
         {
             if (curParam.Equals(Constants.INV_ITEMS))
             {
-                /*
-                 * DataGridTextColumn c1 = new DataGridTextColumn();
-                            c1.Header = "Num";
-                            c1.Binding = new Binding("Num");
-                            c1.Width = 110;
-                            dataGrid1.Columns.Add(c1);
-                            DataGridTextColumn c2 = new DataGridTextColumn();
-                            c2.Header = "Start";
-                            c2.Width = 110;
-                            c2.Binding = new Binding("Start");
-                            dataGrid1.Columns.Add(c2);
-                            DataGridTextColumn c3 = new DataGridTextColumn();
-                            c3.Header = "Finich";
-                            c3.Width = 110;
-                            c3.Binding = new Binding("Finich");
-                            dataGrid1.Columns.Add(c3);
+                
+                DataGridTextColumn c1 = new DataGridTextColumn();
+                c1.Header = "Cod";
+                c1.Binding = new Binding("Cod");
+                c1.Width = 110;
+                dgParamsData.Columns.Add(c1);
+                DataGridTextColumn c2 = new DataGridTextColumn();
+                c2.Header = "Item";
+                c2.Width = 110;
+                c2.Binding = new Binding("Item");
+                dgParamsData.Columns.Add(c2);
 
-                            dataGrid1.Items.Add(new Item() { Num = 1, Start = "2012, 8, 15", Finich = "2012, 9, 15" });
-                            dataGrid1.Items.Add(new Item() { Num = 2, Start = "2012, 12, 15", Finich = "2013, 2, 1" });
-                            dataGrid1.Items.Add(new Item() { Num = 3, Start = "2012, 8, 1", Finich = "2012, 11, 15" });
+                dgParamsData.Items.Add(new string[2] { "6303", "Estacionamientos" });
+                dgParamsData.Items.Add(new string[2] { "6305", "lxhADSLHa" });
+                dgParamsData.Items.Add(new string[2] { "6333", "dcksahkah" });
 
-
-                 */
+                 
 
             }
             else if (curParam.Equals(Constants.INV_AREAS))
