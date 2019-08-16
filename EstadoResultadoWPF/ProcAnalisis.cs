eb3f7e672706a31d34a90f5bce00955c65a2d2e7
Pablo Antonioletti <pablo.antonioletti@gmail.com>
2019-08-16 00:10:51 -0400

    Inicio de proceso de planillas de analisis

---
+++ EstadoResultadoWPF/ProcAnalisis.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace EstadoResultadoWPF
+{
+    public class AnalisisSS
+    {
+        public AnalisisSS(List<String> files)
+        {
+            foreach (String f in files)
+            {
+
+            }
+        }
+    }
+}
